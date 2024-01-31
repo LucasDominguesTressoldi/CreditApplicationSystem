a9e922f3a4767c64c14a58769b4223770a3ab46f
LucasT <tressoldi.lucas@gmail.com>
2024-01-31 15:04:18 -0300

    create CustomerView and add methods to CustomerResource

--- src/main/kotlin/me/dio/credit/application/system/controller/CustomerResource.kt
+++ src/main/kotlin/me/dio/credit/application/system/controller/CustomerResource.kt
@@ -1,7 +1,12 @@
 package me.dio.credit.application.system.controller
 
 import me.dio.credit.application.system.dto.CustomerDto
+import me.dio.credit.application.system.dto.CustomerView
+import me.dio.credit.application.system.entity.Customer
 import me.dio.credit.application.system.service.impl.CustomerService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,4 +20,13 @@ class CustomerResource(private val customerService: CustomerService) {
         val savedCustomer = this.customerService.save(customerDto.toEntity())
         return "Customer ${savedCustomer.email} saved!"
     }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        return CustomerView(customer)
+    }
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: Long) = this.customerService.delete(id)
 }
