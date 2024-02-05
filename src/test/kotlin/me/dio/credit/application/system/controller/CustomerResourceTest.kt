f6af9b3f09a5f30eb9f094c229da8a5f0ef8948a
LucasT <tressoldi.lucas@gmail.com>
2024-02-05 15:53:03 -0300

    create test to invalid inputs like firstName and cpf in CustomerResourceTest

--- src/test/kotlin/me/dio/credit/application/system/controller/CustomerResourceTest.kt
+++ src/test/kotlin/me/dio/credit/application/system/controller/CustomerResourceTest.kt
@@ -1,15 +1,22 @@
 package me.dio.credit.application.system.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import me.dio.credit.application.system.dto.CustomerDto
 import me.dio.credit.application.system.repository.CustomerRepository
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.math.BigDecimal
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -34,4 +41,64 @@ class CustomerResourceTest {
 
     @AfterEach
     fun tearDown() = customerRepository.deleteAll()
+
+    @Test
+    fun `should create customer and return 201 status`() {
+        //given
+        val customerDto: CustomerDto = buildCustomerDto()
+        val valueAsString: String = objectMapper.writeValueAsString(customerDto)
+        //when
+        //then
+        mockMvc.perform(
+            MockMvcRequestBuilders.post(URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(valueAsString)
+        ).andExpect(MockMvcResultMatchers.status().isCreated).andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    fun `should not have a customer with same CPF and return 409 status`() {
+        //given
+        customerRepository.save(buildCustomerDto().toEntity())
+        val customerDto: CustomerDto = buildCustomerDto()
+        val valueAsString: String = objectMapper.writeValueAsString(customerDto)
+        //when
+        //then
+        mockMvc.perform(
+            MockMvcRequestBuilders.post(URL).contentType(MediaType.APPLICATION_JSON)
+                .content(valueAsString)
+        ).andExpect(MockMvcResultMatchers.status().isConflict).andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    fun `should not have a customer with firstName empty and return 409 status`() {
+        //given
+        val customerDto: CustomerDto = buildCustomerDto(firstName = "")
+        val valueAsString: String = objectMapper.writeValueAsString(customerDto)
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post(URL).content(valueAsString)
+            .contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isBadRequest)
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    private fun buildCustomerDto(
+        firstName: String = "Lucas",
+        lastName: String = "Tressoldi",
+        cpf: String = "431.194.775-59",
+        email: String = "lucasEmail@gmail.com",
+        income: BigDecimal = BigDecimal.valueOf(1000.0),
+        password: String = "123",
+        zipCode: String = "13844-023",
+        street: String = "Rua do Lucas"
+    ) = CustomerDto(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        income = income,
+        password = password,
+        zipCode = zipCode,
+        street = street
+    )
 }
