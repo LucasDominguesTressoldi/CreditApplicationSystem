613202884a5c22b38791a8936144f7d0807ab270
LucasT <tressoldi.lucas@gmail.com>
2024-02-05 16:30:47 -0300

    create update tests in CustomerResourceTest

--- src/test/kotlin/me/dio/credit/application/system/controller/CustomerResourceTest.kt
+++ src/test/kotlin/me/dio/credit/application/system/controller/CustomerResourceTest.kt
@@ -2,6 +2,7 @@ package me.dio.credit.application.system.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import me.dio.credit.application.system.dto.CustomerDto
+import me.dio.credit.application.system.dto.CustomerUpdateDto
 import me.dio.credit.application.system.entity.Customer
 import me.dio.credit.application.system.repository.CustomerRepository
 import org.junit.jupiter.api.AfterEach
@@ -132,6 +133,34 @@ class CustomerResourceTest {
         ).andExpect(MockMvcResultMatchers.status().isBadRequest).andDo(MockMvcResultHandlers.print())
     }
 
+    @Test
+    fun `should update a customer and return 200 status`() {
+        //given
+        val customer: Customer = customerRepository.save(buildCustomerDto().toEntity())
+        val customerUpdateDto: CustomerUpdateDto = buildCustomerUpdateDto()
+        val valueAsString: String = objectMapper.writeValueAsString(customerUpdateDto)
+        //when
+        //then
+        mockMvc.perform(
+            MockMvcRequestBuilders.patch("$URL?customerId=${customer.id}").contentType(MediaType.APPLICATION_JSON)
+                .content(valueAsString)
+        ).andExpect(MockMvcResultMatchers.status().isOk).andDo(MockMvcResultHandlers.print())
+    }
+
+    @Test
+    fun `should not update a customer with invalid id and return 400 status`() {
+        //given
+        val invalidId: Long = Random().nextLong()
+        val customerUpdateDto: CustomerUpdateDto = buildCustomerUpdateDto()
+        val valueAsString: String = objectMapper.writeValueAsString(customerUpdateDto)
+        //when
+        //then
+        mockMvc.perform(
+            MockMvcRequestBuilders.patch("$URL?customerId=${invalidId}").contentType(MediaType.APPLICATION_JSON)
+                .content(valueAsString)
+        ).andExpect(MockMvcResultMatchers.status().isBadRequest).andDo(MockMvcResultHandlers.print())
+    }
+
     private fun buildCustomerDto(
         firstName: String = "Lucas",
         lastName: String = "Tressoldi",
@@ -148,6 +177,20 @@ class CustomerResourceTest {
         email = email,
         income = income,
         password = password,
+        zipCode = zipCode,
+        street = street
+    )
+
+    private fun buildCustomerUpdateDto(
+        firstName: String = "Lucas",
+        lastName: String = "Tressoldi",
+        income: BigDecimal = BigDecimal.valueOf(5000.0),
+        zipCode: String = "13844-023",
+        street: String = "Rua do Lucas"
+    ): CustomerUpdateDto = CustomerUpdateDto(
+        firstName = firstName,
+        lastName = lastName,
+        income = income,
         zipCode = zipCode,
         street = street
     )
